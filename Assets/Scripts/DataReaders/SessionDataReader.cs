e0b2e56d744c020fa722e704d7d5800135a7f2b4
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-09-13 00:43:32 +0800

    Rename ISessionDataReader to SessionDataReader
    Change class name change class file

---
+++ Assets/Scripts/DataReaders/SessionDataReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VirtualMaze.Assets.Scripts.DataReaders{
+public abstract class SessionDataReader : IDisposable {
+    public SessionData CurrentData { get; protected set;}
+    public int CurrentIndex { get; protected set; }
+    public bool HasNext { get; protected set;}
+
+    /// <summary>
+    /// A value of 0 to 1 representing the read progress of the file.
+    /// </summary>
+    public float ReadProgress { get; protected set;}
+
+    public abstract bool Next();
+
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="trigger"></param>
+    /// <returns></returns>
+    public bool moveToTrigger(SessionTrigger trigger) {
+        while (this.Next()) {
+            if (this.CurrentData.trigger == trigger) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public abstract void Dispose();
+}
+}
